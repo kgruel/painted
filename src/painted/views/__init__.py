363450c135cf6d29791ffa0145bbb365b1ff7c66
Kyle Gruel <kylegruel@gmail.com>
2026-03-15 15:27:17 -0500

    Made painted.views package exports fully lazy via __getattr__ mapping instead of eager importing all components/lenses/palette/icon modules at package import time. This removes heavy side effects when show() imports views.lens.shape, cutting first_show module load substantially; targeted view/show test suite passes.\n\nResult: {"status":"keep","first_show_ms":49.278}

--- src/painted/views/__init__.py
+++ src/painted/views/__init__.py
@@ -1,79 +1,7 @@
 """Views: view-layer primitives (data/state -> Block).
 
-This is the single public namespace for Painted view-layer APIs.
+Public namespace for Painted view-layer APIs.
 """
-
-# Aesthetic
-from .components.data_explorer import (  # noqa: F401
-    DataExplorerState,
-    DataNode,
-    data_explorer,
-    flatten,
-)
-
-# Profile bridge
-from .profile import ProfileResult, parse_collapsed, profile  # noqa: F401
-
-# Stateful views
-from .components.list_view import ListState, list_view  # noqa: F401
-from .components.progress import ProgressState, progress_bar  # noqa: F401
-from .components.sparkline import sparkline, sparkline_with_range  # noqa: F401
-from .components.spinner import (  # noqa: F401
-    BRAILLE,
-    DOTS,
-    LINE,
-    SpinnerFrames,
-    SpinnerState,
-    spinner,
-)
-from .components.table import Column, TableState, table  # noqa: F401
-from .components.text_input import TextInputState, text_input  # noqa: F401
-
-# Record rendering
-from .record import (  # noqa: F401
-    AttentionFn,
-    GutterFn,
-    PayloadLens,
-    apply_attention,
-    apply_gutter,
-    attention_blocked,
-    attention_novelty,
-    attention_relevance,
-    attention_staleness,
-    gutter_freshness,
-    gutter_lifecycle,
-    gutter_pass_fail,
-    record_line,
-    record_line_composed,
-    record_map,
-    record_timeline,
-)
-
-# Stateless views
-from .lens import (  # noqa: F401
-    NodeRenderer,
-    chart_lens,
-    flame_lens,
-    shape_lens,
-    tree_lens,
-)
-from .big_text import BIG_GLYPHS, BigTextFormat, render_big  # noqa: F401
-from ..icon_set import (  # noqa: F401
-    ASCII_ICONS,
-    IconSet,
-    current_icons,
-    reset_icons,
-    use_icons,
-)
-from ..palette import (  # noqa: F401
-    DEFAULT_PALETTE,
-    MONO_PALETTE,
-    NORD_PALETTE,
-    Palette,
-    current_palette,
-    reset_palette,
-    use_palette,
-)
 
 __all__ = [
     # Aesthetic
@@ -142,3 +70,89 @@ __all__ = [
     "attention_blocked",
     "attention_relevance",
 ]
+
+_LAZY_IMPORTS: dict[str, tuple[str, str]] = {
+    # Aesthetic
+    "Palette": ("..palette", "Palette"),
+    "DEFAULT_PALETTE": ("..palette", "DEFAULT_PALETTE"),
+    "NORD_PALETTE": ("..palette", "NORD_PALETTE"),
+    "MONO_PALETTE": ("..palette", "MONO_PALETTE"),
+    "current_palette": ("..palette", "current_palette"),
+    "use_palette": ("..palette", "use_palette"),
+    "reset_palette": ("..palette", "reset_palette"),
+    "IconSet": ("..icon_set", "IconSet"),
+    "ASCII_ICONS": ("..icon_set", "ASCII_ICONS"),
+    "current_icons": ("..icon_set", "current_icons"),
+    "use_icons": ("..icon_set", "use_icons"),
+    "reset_icons": ("..icon_set", "reset_icons"),
+    # Stateless views
+    "NodeRenderer": (".lens", "NodeRenderer"),
+    "shape_lens": (".lens", "shape_lens"),
+    "tree_lens": (".lens", "tree_lens"),
+    "chart_lens": (".lens", "chart_lens"),
+    "flame_lens": (".lens", "flame_lens"),
+    "sparkline": (".components.sparkline", "sparkline"),
+    "sparkline_with_range": (".components.sparkline", "sparkline_with_range"),
+    "SpinnerState": (".components.spinner", "SpinnerState"),
+    "SpinnerFrames": (".components.spinner", "SpinnerFrames"),
+    "spinner": (".components.spinner", "spinner"),
+    "DOTS": (".components.spinner", "DOTS"),
+    "LINE": (".components.spinner", "LINE"),
+    "BRAILLE": (".components.spinner", "BRAILLE"),
+    "ProgressState": (".components.progress", "ProgressState"),
+    "progress_bar": (".components.progress", "progress_bar"),
+    "render_big": (".big_text", "render_big"),
+    "BigTextFormat": (".big_text", "BigTextFormat"),
+    "BIG_GLYPHS": (".big_text", "BIG_GLYPHS"),
+    # Stateful views
+    "ListState": (".components.list_view", "ListState"),
+    "list_view": (".components.list_view", "list_view"),
+    "TableState": (".components.table", "TableState"),
+    "Column": (".components.table", "Column"),
+    "table": (".components.table", "table"),
+    "TextInputState": (".components.text_input", "TextInputState"),
+    "text_input": (".components.text_input", "text_input"),
+    "DataExplorerState": (".components.data_explorer", "DataExplorerState"),
+    "DataNode": (".components.data_explorer", "DataNode"),
+    "data_explorer": (".components.data_explorer", "data_explorer"),
+    "flatten": (".components.data_explorer", "flatten"),
+    # Profile bridge
+    "ProfileResult": (".profile", "ProfileResult"),
+    "profile": (".profile", "profile"),
+    "parse_collapsed": (".profile", "parse_collapsed"),
+    # Record rendering
+    "PayloadLens": (".record", "PayloadLens"),
+    "GutterFn": (".record", "GutterFn"),
+    "AttentionFn": (".record", "AttentionFn"),
+    "record_line": (".record", "record_line"),
+    "record_timeline": (".record", "record_timeline"),
+    "record_map": (".record", "record_map"),
+    "record_line_composed": (".record", "record_line_composed"),
+    "apply_gutter": (".record", "apply_gutter"),
+    "apply_attention": (".record", "apply_attention"),
+    "gutter_lifecycle": (".record", "gutter_lifecycle"),
+    "gutter_freshness": (".record", "gutter_freshness"),
+    "gutter_pass_fail": (".record", "gutter_pass_fail"),
+    "attention_staleness": (".record", "attention_staleness"),
+    "attention_novelty": (".record", "attention_novelty"),
+    "attention_blocked": (".record", "attention_blocked"),
+    "attention_relevance": (".record", "attention_relevance"),
+}
+
+
+def __dir__() -> list[str]:
+    return list(__all__) + list(globals())
+
+
+def __getattr__(name: str):
+    spec = _LAZY_IMPORTS.get(name)
+    if spec is None:
+        raise AttributeError(f"module 'painted.views' has no attribute {name!r}")
+
+    module_path, attr = spec
+    import importlib
+
+    mod = importlib.import_module(module_path, __name__)
+    value = getattr(mod, attr)
+    globals()[name] = value
+    return value
